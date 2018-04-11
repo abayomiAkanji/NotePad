e02d71235aebad545c98f600737257a669bd3081
dammy_abayomi <sweetmumolu>
2018-04-11 09:46:21 +0100

    SplashScreen added.

---
+++ app/src/main/java/com/abayomi/notepad/view/SplashScreenActivity.kt
@@ -0,0 +1,39 @@
+package com.abayomi.notepad.view
+
+import android.content.Intent
+import android.databinding.DataBindingUtil
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.WindowManager
+import com.abayomi.notepad.R
+import com.abayomi.notepad.databinding.ActivitySplashScreenBinding
+
+class SplashScreenActivity : AppCompatActivity() {
+
+    private lateinit var mBinding: ActivitySplashScreenBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
+        mBinding = DataBindingUtil.setContentView(this, R.layout.activity_splash_screen)
+
+        sleepScreen()
+
+    }
+
+    private fun sleepScreen(){
+        object : Thread() {
+            override fun run() {
+                try {
+                    Thread.sleep(3000)
+                } catch (e: InterruptedException) {
+                    e.printStackTrace()
+                } finally {
+                    startActivity(Intent(applicationContext, MainActivity::class.java))
+                }
+            }
+        }.start()
+    }
+
+}
